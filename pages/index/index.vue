59905970a675d8e5093ac6fbb21d8b8e1d062359
Mac <jon_markov@sina.com>
2025-04-16 10:19:02 +0800

    feat:请求接口

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,9 @@
 <template>
 	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<view v-for="(evt,index) in products">1</view>
+
+		<view v-for="(evt,index) in products">
+			<view>{{evt.text}}</view>
+		</view>
 		<uni-link :href="href" :text="href"></uni-link>
 		<view @click.stop="handleNavigateToTargetPage">去登录</view>
 	</view>
@@ -20,24 +21,14 @@
 			}
 		},
 		methods: {
-
-			async loadProducts() {
-				uni.showToast({ title:'111222222' }); // 弹窗测试
-				const response = await u_navlist({
-					page: 1,
-					pageSize: 10
-				})
-				uni.showToast({ title: response.code||'111' }); // 弹窗测试
-				try {
-					const response = await u_navlist({
-						page: 1,
-						pageSize: 10
-					})
+			handleNavlist() {
+				console.log(u_navlist)
+				const response = u_navlist().then(response => {
+					console.log(response)
 					this.products = response.content
-					uni.showToast({ title: response.code }); // 弹窗测试
-				} catch (error) {
-					console.error('获取商品失败', error)
-				}
+
+				})
+
 			},
 			handleNavigateToTargetPage() {
 				uni.navigateTo({
@@ -46,11 +37,10 @@
 			}
 		},
 		onLoad() {
-			
-		
+			this.handleNavlist()
 		},
 		onShow() {
-			this.loadProducts()
+
 
 		}
 	}
