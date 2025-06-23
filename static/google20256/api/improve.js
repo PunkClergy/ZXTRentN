e38035bff075446f4ac0161deaa516d533669e1f
Mac <jon_markov@sina.com>
2025-06-23 16:24:57 +0800

    安装商查询

--- static/google20256/api/improve.js
+++ static/google20256/api/improve.js
@@ -3,6 +3,7 @@ import {
 } from '@/utils/request/http'
 export const u_companyInfo = (data) => http.get('/companyapi/companyInfo', data) //查询资料
 export const u_companyImprove = (data) => http.postFormData('/companyapi/companyImprove', data) //查询资料
+export const u_installerapiList = (data) => http.get('/installerapi/list', data) //查询资料
 
 
 
